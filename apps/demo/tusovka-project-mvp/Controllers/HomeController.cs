9ac7f4e0ba1b0a844c729b17871e747924d27d9e
Viktor Svyatokha <ahydrax@gmail.com>
2019-12-07 19:13:40 +1000

    Added demo

---
+++ apps/demo/tusovka-project-mvp/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Tusovka.Models;
+
+namespace Tusovka.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("calendar")]
+        public IActionResult Calendar()
+        {
+            return View();
+        }
+
+        [Route("event")]
+        public IActionResult Event()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
